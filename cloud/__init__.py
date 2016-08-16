944b6924a56e7ed1246f65e7cce284aec80b8886
Luca Sbardella <luca@quantmind.com>
2016-08-16 14:27:47 +0100

    Release 0.5.1

--- cloud/__init__.py
+++ cloud/__init__.py
@@ -1,6 +1,6 @@
 """Asynchronous clients for AWS and Pusher"""
 from .utils.version import get_version
 
-VERSION = (0, 5, 0, 'final', 0)
+VERSION = (0, 5, 1, 'final', 0)
 
 __version__ = get_version(VERSION, __file__)
