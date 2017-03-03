6222ececd5bf791b1a44f6efd64dac3d776c1977
Luca Sbardella <luca@quantmind.com>
2017-03-03 08:09:13 +0000

    run tests sequentially

--- install-dev.sh
+++ install-dev.sh
@@ -4,3 +4,4 @@ pip install --upgrade pip wheel
 pip install --upgrade setuptools
 pip install -r requirements-dev.txt
 pip install -r requirements.txt
+pip install git+https://github.com/quantmind/pulsar-agile.git
